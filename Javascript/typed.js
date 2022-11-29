b108647c8f018fea585a7aa5f52486a9c7fabf62
Jabed <jabedalimollah39@gmail.com>
2022-11-29 11:08:48 +0530

    first commit

---
+++ Javascript/typed.js
@@ -0,0 +1,10 @@
+var typed = new Typed("#typed", {
+  strings: [
+    "I am  <span class='typed-text-color'>Web Developer</span>",
+    "I am <span class='typed-text-color'>React Developer</span> ",
+    "I am <span class='typed-text-color'>Web Designer</span> ",
+  ],
+  typeSpeed: 50,
+  backSpeed: 100,
+  loop: true,
+});
